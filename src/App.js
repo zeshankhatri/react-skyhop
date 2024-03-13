940501236ccf377931e2681ac7bfb1754959f188
Zeshan Khatri <zeshan.khatri@gmail.com>
2024-03-13 17:20:23 -0400

    Working toggle and completed left section of Modal.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Dropdown from "./components/Dropdown";
+import ToggleSwitch from "./components/ToggleSwitch";
 import { useState } from "react";
 
 function App() {
@@ -96,7 +97,20 @@ function App() {
                   <p className="Success-text">No Elapsed Dates!</p>
                   <hr className="Line"></hr>
                   <p className="Modal-subheading">Tolerence Window:</p>
-                  <div className="Tolerance-window"></div>
+                  <div className="Tolerance-window">
+                    <ToggleSwitch />
+                    <div className="Vertical-line"></div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#293e5c"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 48 48"
+                    >
+                      <path d="M 24 4 C 12.972066 4 4 12.972074 4 24 C 4 35.027926 12.972066 44 24 44 C 35.027934 44 44 35.027926 44 24 C 44 12.972074 35.027934 4 24 4 z M 24 7 C 33.406615 7 41 14.593391 41 24 C 41 33.406609 33.406615 41 24 41 C 14.593385 41 7 33.406609 7 24 C 7 14.593391 14.593385 7 24 7 z M 23.476562 11.978516 A 1.50015 1.50015 0 0 0 22 13.5 L 22 25.5 A 1.50015 1.50015 0 0 0 23.5 27 L 31.5 27 A 1.50015 1.50015 0 1 0 31.5 24 L 25 24 L 25 13.5 A 1.50015 1.50015 0 0 0 23.476562 11.978516 z"></path>
+                    </svg>
+                    <p className="Tolerance-text">Select Tolerance Level</p>
+                  </div>
                 </div>
               </div>
             </div>
